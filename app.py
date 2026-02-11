bfdd6192c7dc7afeb8c9c6f3c1b92c8aeef757fc
Rishivijai <vijairobors@gmail.com>
2026-02-11 14:18:11 +0530

    Fix DB initialization properly

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ app.secret_key = "supersecret"  # required for sessions
 
 # Initialize DB tables if not exist
 
-if not os.path.exists("/tmp/bank.db"):
-    with app.app_context():
-        db = database.get_db()
-        with open("schema.sql", "r") as f:
-            db.executescript(f.read())
+@app.before_first_request
+def initialize_database():
+    db = database.get_db()
+    with open("schema.sql", "r") as f:
+        db.executescript(f.read())
+
 
 # ---------------------------
 # ROUTES
